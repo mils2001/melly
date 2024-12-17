4571252cf957c141582d6b46b9437b078b5e0dbb
chris <Mileschris466@gmail.com>
2024-12-17 09:28:21 +0300

    navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,149 +1,76 @@
-import React, { Component } from 'react';
-import './navbar.css';
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
 
-export class Navbar extends Component {
+function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isResumePopupOpen, setIsResumePopupOpen] = useState(false); // State for controlling the pop-up visibility
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: "Welcome",
-      isSignUpPopupVisible: false,
-      isSignInPopupVisible: false,
-      signUpDetails: { name: "", email: "", password: "" },
-      signInDetails: { email: "", password: "" },
-    };
-  }
-
-  toggleSignUpPopup = () =>
-    this.setState((prevState) => ({
-      isSignUpPopupVisible: !prevState.isSignUpPopupVisible,
-    }));
-
-  toggleSignInPopup = () =>
-    this.setState((prevState) => ({
-      isSignInPopupVisible: !prevState.isSignInPopupVisible,
-    }));
-
-  handleSignUpChange = (event) => {
-    const { name, value } = event.target;
-    this.setState((prevState) => ({
-      signUpDetails: { ...prevState.signUpDetails, [name]: value },
-    }));
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
-  handleSignInChange = (event) => {
-    const { name, value } = event.target;
-    this.setState((prevState) => ({
-      signInDetails: { ...prevState.signInDetails, [name]: value },
-    }));
+  const toggleResumePopup = () => {
+    setIsResumePopupOpen(!isResumePopupOpen);
   };
 
-  handleSignUpSubmit = (event) => {
-    event.preventDefault();
-    const { signUpDetails } = this.state;
-    alert("Sign-Up Successful!");
-    console.log("Sign-Up Details:", signUpDetails);
-    this.toggleSignUpPopup();
-  };
+  return (
+    <div className="container">
+      <nav className="navbar">
+        <div className="container-logo">
+          <img src="https://i.imgur.com/bpo8Jnv.jpeg" alt="logo" className="logo" />
+        </div>
+        <ul className={`nav-links ${isMobileMenuOpen ? 'mobile-open' : ''}`}>
+          <li>
+            <Link to="/">Zoom</Link>
+          </li>
+          <li>
+            <Link to="./pages/Home.js">Home</Link>
+          </li>
+          <li>
+            <Link to="./pages/About.js">About</Link>
+          </li>
+          <li>
+            <Link to="./pages/Contact.js">Contact</Link>
+          </li>
+          <li>
+            <Link to="./pages/experiece.js">Experience</Link>
+          </li>
+         
+        </ul>
+        <div className="btn-container">
+          <button className="btn" onClick={toggleResumePopup}>RESUME</button>
+        </div>
+        <button 
+          className="hamburger-menu" 
+          onClick={toggleMobileMenu}
+          aria-label={isMobileMenuOpen ? "Close menu" : "Open menu"}
+        >
+         
+        </button>
+      </nav>
 
-  handleSignInSubmit = (event) => {
-    event.preventDefault();
-    const { signInDetails } = this.state;
-    alert("Sign-In Successful!");
-    console.log("Sign-In Details:", signInDetails);
-    this.toggleSignInPopup();
-  };
-
-  render() {
-    const { isSignUpPopupVisible, isSignInPopupVisible, signUpDetails, signInDetails } = this.state;
-  
-    return (
-      <div>
-        <nav className='navbar'>
-          <div className='container-logo'>
-            <img src='https://i.imgur.com/lncSbip.jpeg' alt='logo' className='logo' />
-            <span className='title'>Movflix</span>
+      {/* Pop-up Form for Resume */}
+      {isResumePopupOpen && (
+        <div className="popup-form">
+          <div className="popup-content">
+            <h2>Contact Information</h2>
+            <p>If you would like to get in touch, feel free to contact me via:</p>
+            <ul>
+              <li>Email: <strong>mileschris466@gmail.com</strong></li>
+              <li>Phone: <strong>+254748778480</strong></li>
+            </ul>
+            <form>
+              <label htmlFor="message">Your Message:</label>
+              <textarea id="message" rows="4" placeholder="Write your message here..."></textarea>
+              <button type="submit" className="submit-btn">Send Message</button>
+              <button type="button" className="close-btn" onClick={toggleResumePopup}>Close</button>
+            </form>
           </div>
-          <ul className='nav-links'>
-            <li>
-              <Link to='./Pages/Home.js'>Home</Link>
-            </li>
-            <li>
-              <Link to='./Pages/About.js'>About</Link>
-            </li>
-            <li>
-              <Link to='./Pages/Contact.js'>Contact</Link>
-            </li>
-          </ul>
-          <div className='container'>
-            <button className='btn' onClick={this.toggleSignUpPopup}>Sign Up</button>
-            <button className='btn' onClick={this.toggleSignInPopup}>Sign In</button>
-          </div>
-        </nav>
-
-        {isSignUpPopupVisible && (
-          <div className="popup-overlay">
-            <div className="popup">
-              <h3>Sign Up</h3>
-              <form onSubmit={this.handleSignUpSubmit}>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Name"
-                  value={signUpDetails.name}
-                  onChange={this.handleSignUpChange}
-                />
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  value={signUpDetails.email}
-                  onChange={this.handleSignUpChange}
-                />
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  value={signUpDetails.password}
-                  onChange={this.handleSignUpChange}
-                />
-                <button type="submit">Submit</button>
-                <button type="button" onClick={this.toggleSignUpPopup}>Close</button>
-              </form>
-            </div>
-          </div>
-        )}
-
-        {isSignInPopupVisible && (
-          <div className="popup-overlay">
-            <div className="popup">
-              <h3>Sign In</h3>
-              <form onSubmit={this.handleSignInSubmit}>
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  value={signInDetails.email}
-                  onChange={this.handleSignInChange}
-                />
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  value={signInDetails.password}
-                  onChange={this.handleSignInChange}
-                />
-                <button type="submit">Submit</button>
-                <button type="button" onClick={this.toggleSignInPopup}>Close</button>
-              </form>
-            </div>
-          </div>
-        )}
-
-      </div>
-    );
-  }
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Navbar;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, from react;
-import './App.css';
-import Navbar from '../components/navbar';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Zoom from './Zoom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* Define proper paths */}
+          <Route path="/" element={<Zoom />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-export default App
+export default App;
+
+
+
