bc1045444f4db46580d0b8e71db509ea4bd07b58
chris <Mileschris466@gmail.com>
2024-12-20 09:14:03 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes,Link  } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes, Link } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Zoom from './components/Zoom';
-import Home from './components/pages/Home'
-import About from './components/pages/About'
-import Contact from './components/pages/Contact'
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
     <div className="App">
-      
-        <Zoom/>
-        <Navbar/>
-        <Router>
-      <nav>
-        <Link to="./components/pages/Home.js">Home</Link> | <Link to="./components/pages/About.js">About</Link> | <Link to="./components/pages/Contact.js">Contact</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Zoom />
+        <Navbar />
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/contact">Contact</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
 
 
 
