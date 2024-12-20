6f4949bc185fc5af0f9ae938a2b95923ccfe427e
chris <Mileschris466@gmail.com>
2024-12-20 09:04:50 +0300

    router

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import { Route, BrowserRouter as Router, Routes,  } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Zoom from './components/Zoom';
 import Home from './components/pages/Home'
+import About from './components/pages/About'
+import Contact from './components/pages/Contact'
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      
         <Zoom/>
         <Navbar/>
-        <Routes>
-          <Route path='./components/pages/Home.js' element={<Home/>}/>
-          
-        </Routes>
-      </Router>
+        <Router>
+      <nav>
+        <Link to="./components/pages/Home.js">Home</Link> | <Link to="./components/pages/About.js">About</Link> | <Link to="./components/pages/Contact.js">Contact</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
