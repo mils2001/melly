7769a12aa2967db68c7d90034756bbd3bfbcfb29
chris <Mileschris466@gmail.com>
2024-12-20 09:16:28 +0300

    debugging

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes, Link } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes,  } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Zoom from './components/Zoom';
 import Home from './components/pages/Home';
@@ -12,9 +12,7 @@ function App() {
       <Router>
         <Zoom />
         <Navbar />
-        <nav>
-          <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/contact">Contact</Link>
-        </nav>
+      
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
